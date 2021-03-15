3ad6184b9182bf729f2ff396b6539c48409d55d9
Mubarak Ahmad <mubarakahmad@gmail.com>
2021-03-15 19:38:54 +0500

    use compiled index.js

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ try {
 	// Get the JSON webhook payload for the event that triggered the workflow
 	const payload = JSON.stringify(github.context.payload, undefined, 2);
 	console.log(`The event payload: ${payload}`);
+
+	const ver = core.getInput('version_of_release');
+	console.log(`eeeee ${ver}!`);
+	core.setOutput("ver", ver);
+	// Get the JSON webhook payload for the event that triggered the workflow
+	const payload = JSON.stringify(github.context.payload, undefined, 2);
+	console.log(`The event payload2: ${payload}`);
 } catch (error) {
 	core.setFailed(error.message);
 }
