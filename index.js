159e2d0330d27afbf3827c1aa851beb2472efd67
Mubarak Ahmad <mubarakahmad@gmail.com>
2021-03-15 20:12:37 +0500

    use compiled index.js

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-	
-	const ver = core.getInput('version_of_release');
-	console.log(`eeeee ${ver}!`);
-	core.setOutput("ver", ver);
+	// `who-to-greet` input defined in action metadata file
+	const nameToGreet = core.getInput('who-to-greet');
+	console.log(`Hello ${nameToGreet}!`);
+	const time = (new Date()).toTimeString();
+	core.setOutput("time", time);
 	// Get the JSON webhook payload for the event that triggered the workflow
 	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`The event payload2: ${payload}`);
+	console.log(`The event payload: ${payload}`);
+
+	const ver = core.getInput('version_of_release');
+	console.log(`Hello ${ver}!`);
+	core.setOutput("time", ver);
 } catch (error) {
 	core.setFailed(error.message);
 }
