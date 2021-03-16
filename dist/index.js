0664022cb7933940a8113db647689f0bda75989e
Mubarak Ahmad <mubarakahmad@gmail.com>
2021-03-16 09:44:17 +0500

    use compiled index.js

--- dist/index.js
+++ dist/index.js
@@ -10,14 +10,14 @@ const github = __nccwpck_require__(438);
 
 try {
 	// `who-to-greet` input defined in action metadata file
-	const nameToGreet = core.getInput('who-to-greet');
-	console.log(`Hello ${nameToGreet}!`);
-	const time = (new Date()).toTimeString();
-	core.setOutput("time", time);
-	// Get the JSON webhook payload for the event that triggered the workflow
-	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`The event payload: ${payload}`);
-
+	// const nameToGreet = core.getInput('who-to-greet');
+	// console.log(`Hello ${nameToGreet}!`);
+	// const time = (new Date()).toTimeString();
+	// core.setOutput("time", time);
+	// // Get the JSON webhook payload for the event that triggered the workflow
+	// const payload = JSON.stringify(github.context.payload, undefined, 2);
+	// console.log(`The event payload: ${payload}`);
+	console.log(`Before GetInput`);
 	const version = core.getInput('version_of_release');
 	console.log(`Hello ${version}!`);
 	core.setOutput("ver", version);
--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const github = require('@actions/github');
 
 try {
 	// `who-to-greet` input defined in action metadata file
-	const nameToGreet = core.getInput('who-to-greet');
-	console.log(`Hello ${nameToGreet}!`);
-	const time = (new Date()).toTimeString();
-	core.setOutput("time", time);
-	// Get the JSON webhook payload for the event that triggered the workflow
-	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`The event payload: ${payload}`);
-
+	// const nameToGreet = core.getInput('who-to-greet');
+	// console.log(`Hello ${nameToGreet}!`);
+	// const time = (new Date()).toTimeString();
+	// core.setOutput("time", time);
+	// // Get the JSON webhook payload for the event that triggered the workflow
+	// const payload = JSON.stringify(github.context.payload, undefined, 2);
+	// console.log(`The event payload: ${payload}`);
+	console.log(`Before GetInput`);
 	const version = core.getInput('version_of_release');
 	console.log(`Hello ${version}!`);
 	core.setOutput("ver", version);
