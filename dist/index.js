5cf77a441096afe8cae10536320fea9f122d7a9d
Mubarak Ahmad <mubarakahmad@gmail.com>
2021-04-19 15:18:54 +0500

    v1

--- dist/index.js
+++ dist/index.js
@@ -7,23 +7,40 @@ module.exports =
 
 const core = __nccwpck_require__(186);
 const github = __nccwpck_require__(438);
+const yaml = __nccwpck_require__(350);
+const fs = __nccwpck_require__(747);
 
+// Get document, or throw exception on error
 try {
-	// `who-to-greet` input defined in action metadata file
-	const nameToGreet = core.getInput('who-to-greet');
-	console.log(`Hello ${nameToGreet}!`);
-	const time = (new Date()).toTimeString();
-	core.setOutput("time", time);
-	// Get the JSON webhook payload for the event that triggered the workflow
-	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`The event payload: ${payload}`);
-
-	const version = core.getInput('version_of_release');
-	console.log(`Hello ${version}!`);
-	core.setOutput("ver", version);
-} catch (error) {
-	core.setFailed(error.message);
+	const doc = yaml.load(fs.readFileSync('release.yml', 'utf8'));
+	
+	// const version = core.getInput('version_of_release');
+	// console.log(`Hello ${version}!`);
+	core.setOutput("ver", doc);
+	console.log(doc);
+} catch (e) {
+	console.log(e);
 }
+
+
+// try {
+// 	// `who-to-greet` input defined in action metadata file
+// 	const nameToGreet = core.getInput('who-to-greet');
+// 	console.log(`Hello ${nameToGreet}!`);
+// 	const time = (new Date()).toTimeString();
+// 	core.setOutput("time", time);
+// 	// Get the JSON webhook payload for the event that triggered the workflow
+// 	const payload = JSON.stringify(github.context.payload, undefined, 2);
+// 	console.log(`The event payload: ${payload}`);
+
+// 	const version = core.getInput('version_of_release');
+// 	console.log(`Hello ${version}!`);
+// 	core.setOutput("ver", version);
+// } catch (error) {
+// 	core.setFailed(error.message);
+// }
+
+
 
 
 /***/ }),
@@ -5858,6 +5875,14 @@ function wrappy (fn, cb) {
 /***/ ((module) => {
 
 module.exports = eval("require")("encoding");
+
+
+/***/ }),
+
+/***/ 350:
+/***/ ((module) => {
+
+module.exports = eval("require")("js-yaml");
 
 
 /***/ }),
--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const yaml = require('js-yaml');
+const fs = require('fs');
 
+// Get document, or throw exception on error
 try {
-	// `who-to-greet` input defined in action metadata file
-	const nameToGreet = core.getInput('who-to-greet');
-	console.log(`Hello ${nameToGreet}!`);
-	const time = (new Date()).toTimeString();
-	core.setOutput("time", time);
-	// Get the JSON webhook payload for the event that triggered the workflow
-	const payload = JSON.stringify(github.context.payload, undefined, 2);
-	console.log(`The event payload: ${payload}`);
-
-	const version = core.getInput('version_of_release');
-	console.log(`Hello ${version}!`);
-	core.setOutput("ver", version);
-} catch (error) {
-	core.setFailed(error.message);
+	const doc = yaml.load(fs.readFileSync('release.yml', 'utf8'));
+	
+	// const version = core.getInput('version_of_release');
+	// console.log(`Hello ${version}!`);
+	core.setOutput("ver", doc);
+	console.log(doc);
+} catch (e) {
+	console.log(e);
 }
+
+
+// try {
+// 	// `who-to-greet` input defined in action metadata file
+// 	const nameToGreet = core.getInput('who-to-greet');
+// 	console.log(`Hello ${nameToGreet}!`);
+// 	const time = (new Date()).toTimeString();
+// 	core.setOutput("time", time);
+// 	// Get the JSON webhook payload for the event that triggered the workflow
+// 	const payload = JSON.stringify(github.context.payload, undefined, 2);
+// 	console.log(`The event payload: ${payload}`);
+
+// 	const version = core.getInput('version_of_release');
+// 	console.log(`Hello ${version}!`);
+// 	core.setOutput("ver", version);
+// } catch (error) {
+// 	core.setFailed(error.message);
+// }
+
+
